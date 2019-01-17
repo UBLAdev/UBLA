94bcece69e46397725382c3bdb66ffa6892e4f8e
Jona Heitzer# <s_heitzer@stud.her-berlin.de>
2019-01-17 14:47:34 +0100

    Implemented LocationComponent, works fine.

--- app/src/main/java/ubla/org/ubla/MainActivity.java
+++ app/src/main/java/ubla/org/ubla/MainActivity.java
@@ -1,6 +1,6 @@
 package ubla.org.ubla;
 
-import android.location.LocationManager;
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,19 +9,29 @@ import android.util.Log;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener, Style.OnStyleLoaded {
 
     // Constants
     private static final String TAG = "MainActivity";
 
     // MapView Object
     private MapView mapView;
+
+    // MapboxMap stuff
+    MapboxMap mapboxMap;
+
+    // LocationComponent stuff
+    LocationComponent locationComponent;
 
     // Permission Stuff
     private PermissionsManager permissionsManager;
@@ -49,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private void getPermissions() {
         permissionsManager = new PermissionsManager(this);
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            // TODO: LocationComponent must be called here
+            return;
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
@@ -57,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-    //// ---------- LocationListener Callbacks implementation ---------- ////
+    //// ---------- PermissionListener Callbacks implementation ---------- ////
 
     @Override
     public void onExplanationNeeded(List<String> permissionsToExplain) {
@@ -67,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onPermissionResult(boolean granted) {
         if (granted) {
-            // TODO: LocationComponent must be called here
+            Log.i(TAG, "User has granted Permissions.");
         } else {
             Log.e(TAG, "User has not granted permissions.");
         }
@@ -79,11 +89,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    // ---------- OnMapReadyCallback implementation ---------- //
+    // ---------- LocationComponent stuff ---------- //
+
+    // must only be called if permission is already granted and checked!
+    @SuppressLint("MissingPermission")
+    @SuppressWarnings( {"Missing Permission"} ) // due to permission handling with Mapbox Core util
+    private void initLocationComponent() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            locationComponent = mapboxMap.getLocationComponent();
+
+            //TODO: Options can be set here
+            // Activate LocationComponent without setting options
+            locationComponent.activateLocationComponent(this, mapboxMap.getStyle());
+
+            // Enable to make visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set Camera Mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            // Set Render Mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    // TODO: What else has to be set?
+    public void initMapboxMap() {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, (Style.OnStyleLoaded) this);
+        Log.i(TAG, "initMapBoxMap() has finished.");
+        // Log.i(TAG, mapboxMap.getStyle().toString());
+    }
+
+    //// ---------- OnMapReadyCallback implementation ---------- //
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        initMapboxMap();
+    }
 
+    //// ---------- OnStyleLoaded implementation ---------- //
+
+    @Override
+    public void onStyleLoaded(@NonNull Style style) {
+        Log.i(TAG, "entered onStyleLoaded");
+        initLocationComponent();
     }
 
     // ---------- AppCompatActivity Lifecycle Callbacks implementation ---------- //
