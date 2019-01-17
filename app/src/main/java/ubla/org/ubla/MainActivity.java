929c591a1db0aa241862a001bc58114e207aa9ed
Jona Heitzer# <s_heitzer@stud.her-berlin.de>
2019-01-17 13:41:51 +0100

    Implemented PermissionManager and Listener to get Location Permissions.

--- app/src/main/java/ubla/org/ubla/MainActivity.java
+++ app/src/main/java/ubla/org/ubla/MainActivity.java
@@ -1,23 +1,37 @@
 package ubla.org.ubla;
 
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
+
+    // Constants
+    private static final String TAG = "MainActivity";
 
     // MapView Object
     private MapView mapView;
 
+    // Permission Stuff
+    private PermissionsManager permissionsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // handle Permissions
+        getPermissions();
 
         // get Instance of MapView
         // TODO: Refactor hardcoded API key!!
@@ -27,18 +41,52 @@ public class MainActivity extends AppCompatActivity {
         // get MapView and prepare it for use
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        // Map adjustments, Style and other stuff can be done here
-                    }
-                });
-            }
-        });
+        mapView.getMapAsync(this);
     }
+
+    // ---------- Handle Permission Stuff ---------- //
+
+    private void getPermissions() {
+        permissionsManager = new PermissionsManager(this);
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // TODO: LocationComponent must be called here
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+
+    //// ---------- LocationListener Callbacks implementation ---------- ////
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            // TODO: LocationComponent must be called here
+        } else {
+            Log.e(TAG, "User has not granted permissions.";
+        }
+    }
+
+    // TODO: Is this necessary??
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // ---------- OnMapReadyCallback implementation ---------- //
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+
+    }
+
+    // ---------- AppCompatActivity Lifecycle Callbacks implementation ---------- //
 
     @Override
     public void onStart() {
